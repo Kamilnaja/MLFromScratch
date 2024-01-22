8e710d92f048037f7c76fb8f0be018408a2d08af
kamilnaja <kamilnaja@gmail.com>
2024-01-22 19:37:42 +0100

    iterator

--- initialize.py
+++ initialize.py
@@ -10,3 +10,19 @@ n_neurons = 4
 
 weights = 0.01 * np.random.randint(n_inputs, n_neurons)
 print(a)
+
+print(a.isalpha())
+
+M = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+col2 = [row[0] + 4 for row in M]
+
+mielonka = 'mielonka'
+
+letter = [letter * 2 for letter in mielonka]
+
+hello = 'hello'
+helloLetter = (le + ' ' for le in hello)
+
+print(next(helloLetter))
+print(next(helloLetter))
+print(next(helloLetter))
